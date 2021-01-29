f82d1d5251ef4d5b4f0c311e3c7b172f50ef68da
vbedrik <viktoriyabedrik@gmail.com>
2021-01-29 09:23:44 -0500

    more work

--- src/test/java/FeatureACode.java
+++ src/test/java/FeatureACode.java
@@ -1,5 +1,6 @@
 public class FeatureACode {
     public static void main(String[] args) {
         System.out.println("Awesome work for A started");
+        System.out.println("B21 is learning mor gir");
     }
 }
